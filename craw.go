476d7441c2bbebecb2e9133f5e3c8ad7a5b7cac9
Gustavo Alexandre Nobre Mesquita <gustavoalexandredev@gmail.com>
2021-10-03 00:05:49 -0300

    chore: encapsule fetch article

--- craw.go
+++ craw.go
@@ -34,48 +34,50 @@ func Fetch(url, name string, txtSlince chan []string) {
 		return
 	}
 	// Articles
-	rawArticles := doc.Find("#mw-content-text > div > p").FilterFunction(func(i int, s *goquery.Selection) bool {
-		t := s.Text()
-		// Removing junk data
-		return len(t) >= 10
-	})
+	func() {
+		rawArticles := doc.Find("#mw-content-text > div > p").FilterFunction(func(i int, s *goquery.Selection) bool {
+			t := s.Text()
+			// Removing junk data
+			return len(t) >= 10
+		})
 
-	rawTxtArticles := rawArticles.Text()
+		rawTxtArticles := rawArticles.Text()
 
-	// The page not respond with 404, so if not has "." the url fail
-	if !strings.Contains(rawTxtArticles, ".") {
-		failFetch(url)
-		return
-	}
-
-	// Removing junks
-	rawTxtArticles = strings.ReplaceAll(rawTxtArticles, "\t", "")
-	rawTxtArticles = strings.ReplaceAll(rawTxtArticles, "\r", "")
-	rawTxtArticles = strings.ReplaceAll(rawTxtArticles, "  ", "")
-
-	// by '\n'
-	splitted := strings.Split(rawTxtArticles, "\n")
-	// by '.'
-	// splitted := strings.Split(rawTxtArticles, ".")
-
-	// Check if page not exists
-	if strings.Contains(splitted[0], "There is currently no text in this page") {
-		failFetch(url)
-		return
-	}
-
-	for i, v := range splitted {
-		isSmall := len(strings.Join(atricles, "")) < 2500
-		if i < ARTICLES_TO_TAKE || isSmall {
-			txt := v
-			txt = strings.TrimSpace(txt)
-
-			// Removing junk as [23] or [7], I don't know what is it
-			re := regexp.MustCompile(`(\[)+\d+(\])`)
-			txtClean1 := re.ReplaceAllString(txt, "") + "."
-			atricles = append(atricles, txtClean1)
+		// The page not respond with 404, so if not has "." the url fail
+		if !strings.Contains(rawTxtArticles, ".") {
+			failFetch(url)
+			return
 		}
-	}
+
+		// Removing junks
+		rawTxtArticles = strings.ReplaceAll(rawTxtArticles, "\t", "")
+		rawTxtArticles = strings.ReplaceAll(rawTxtArticles, "\r", "")
+		rawTxtArticles = strings.ReplaceAll(rawTxtArticles, "  ", "")
+
+		// by '\n'
+		splitted := strings.Split(rawTxtArticles, "\n")
+		// by '.'
+		// splitted := strings.Split(rawTxtArticles, ".")
+
+		// Check if page not exists
+		if strings.Contains(splitted[0], "There is currently no text in this page") {
+			failFetch(url)
+			return
+		}
+
+		for i, v := range splitted {
+			isSmall := len(strings.Join(atricles, "")) < 2500
+			if i < ARTICLES_TO_TAKE || isSmall {
+				txt := v
+				txt = strings.TrimSpace(txt)
+
+				// Removing junk as [23] or [7], I don't know what is it
+				re := regexp.MustCompile(`(\[)+\d+(\])`)
+				txtClean1 := re.ReplaceAllString(txt, "") + "."
+				atricles = append(atricles, txtClean1)
+			}
+		}
+	}()
 
 	// println("SIZE OF", name, "->", len(atricles))
 	// println("FIRST OF", name, "HAS", "->", len(atricles[0]))
